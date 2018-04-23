339f2d3906c32fa9d311af03fe82970593e5806b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/UserViewVariants/Module.cs
@@ -0,0 +1,11 @@
+using System;
+using DevExpress.ExpressApp;
+using System.Collections.Generic;
+
+namespace UserViewVariants {
+    public sealed partial class UserViewVariantsModule : ModuleBase {
+        public UserViewVariantsModule() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VB/UserViewVariants/Module.vb
@@ -0,0 +1,13 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.ExpressApp
+Imports System.Collections.Generic
+
+Namespace UserViewVariants
+	Public NotInheritable Partial Class UserViewVariantsModule
+		Inherits ModuleBase
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
